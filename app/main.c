5bba0e7f82e5ceafada4244e1802d16e20d47de5
PierceRodriguez1 <122406550+PierceRodriguez1@users.noreply.github.com>
2024-09-17 18:27:17 +0000

    task 4 complete and code setup correctly now

--- app/main.c
+++ app/main.c
@@ -2,32 +2,57 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
+#include "../src/lab.h"
+#include <readline/readline.h>
+#include <readline/history.h>
 
-int main(int argc, char ""argv)
-{
-  printf("hello world\n");
+// Function to handle command-line arguments using getopt
+void handle_getopt(int argc, char *argv[]) {
+    int c;
+    while((c = getopt(argc, argv, "abc:v")) != -1) {
+        switch(c) {
+            case 'a':
+                printf("Option -a selected\n");
+                break;
+            case 'b':
+                printf("Option -b selected\n");
+                break;
+            case 'c':
+                printf("Option -c selected with value %s\n", optarg);
+                break;
+            case 'v':
+                printf("Version %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+                exit(0);
+            case '?':
+                if (optopt == 'c') {
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                } else if (isprint(optopt)) {
+                    fprintf(stderr, "Unknown option `-%c`.\n", optopt);
+                } else {
+                    fprintf(stderr, "Unknown option character `\\x%x`.\n", optopt);
+                }
+                exit(1);
+            default:
+                abort();
+        }
+    }
+}
 
-  int c;
+int main(int argc, char *argv[]) {
+    // Handle getopt logic
+    handle_getopt(argc, argv);
 
-  while((c = getopt(argc, argv, "abc:")) != -1)
-    switch(c)
-  {
-    case 'a':
-     printf("get a here");
-      break;
+    // Initialize Readline for interactive input
+    char *line;
+    using_history();  // Initialize history
 
-    case 'b':
-      printf("get b here");
-      break;
+    while ((line = readline("$ "))) {  // Display "$ " as the prompt
+        if (*line) {  // Check if the line is not empty
+            printf("You entered: %s\n", line);
+            add_history(line);  // Add the line to history for arrow key navigation
+        }
+        free(line);  // Free the allocated memory from readline
+    }
 
-    case 'c':
-      printf("get c here");
-      break;
-
-    case '?':
-      if(isprint(getopt))
-        fprintf(stderr, "Unknown Option", optopt);
-      else
-        fprintf(srderr, "Unkown Option", optopt);
-  }
+    return 0;
 }
