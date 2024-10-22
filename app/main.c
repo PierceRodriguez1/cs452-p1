9f3c6bf03fb8fc43a6a0871cb0c5d9dc8d0d3436
PierceRodriguez1 <122406550+PierceRodriguez1@users.noreply.github.com>
2024-10-22 02:02:39 +0000

    changed to project 2

--- app/main.c
+++ app/main.c
@@ -1,121 +1,225 @@
-#include "../src/lab.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 #include <unistd.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include <signal.h>
+#include <stdbool.h>
+#include <time.h>
+#include <sys/time.h> /* for gettimeofday system call */
+#include "../src/lab.h"
+#include <bits/getopt_core.h>
 
-//#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define UNUSED(x) (void)x
+#define MAX_C 8           /* Maximum number of consumer threads */
+#define MAX_P 8           /* Maximum number of producer threads */
+#define MAX_SLEEP 1000000 /* maximum time a thread can sleep in nanoseconds*/
 
-void initialize_shell(struct shell *sh) {
-    sh->shell_terminal = STDIN_FILENO;
-    sh->shell_is_interactive = isatty(sh->shell_terminal);
+static bool delay = false;
 
-    if (sh->shell_is_interactive) {
-        while (tcgetpgrp(sh->shell_terminal) != (sh->shell_pgid = getpgrp()))
-            kill(-sh->shell_pgid, SIGTTIN);
-
-        signal(SIGINT, SIG_IGN);
-        signal(SIGQUIT, SIG_IGN);
-        signal(SIGTSTP, SIG_IGN);
-        signal(SIGTTIN, SIG_IGN);
-        signal(SIGTTOU, SIG_IGN);
-
-        sh->shell_pgid = getpid();
-        if (setpgid(sh->shell_pgid, sh->shell_pgid) < 0) {
-            perror("Couldn't put the shell in its own process group");
-            exit(1);
-        }
-
-        tcsetpgrp(sh->shell_terminal, sh->shell_pgid);
-        tcgetattr(sh->shell_terminal, &sh->shell_tmodes);
-    }
+double getMilliSeconds()
+{
+     struct timeval now;
+     gettimeofday(&now, (struct timezone *)0);
+     return (double)now.tv_sec * 1000.0 + now.tv_usec / 1000.0;
 }
 
-void launch_process(struct shell *sh, char **cmd) {
-    pid_t pid = fork();
-    if (pid == 0) {
-        // Child process
-        pid_t child = getpid();
-        setpgid(child, child);
-        tcsetpgrp(sh->shell_terminal, child);
-        
-        signal(SIGINT, SIG_DFL);
-        signal(SIGQUIT, SIG_DFL);
-        signal(SIGTSTP, SIG_DFL);
-        signal(SIGTTIN, SIG_DFL);
-        signal(SIGTTOU, SIG_DFL);
+/*Track the total items produced and consumed*/
+static struct
+{
+     unsigned int num;
+     pthread_mutex_t lock;
+} numproduced = {0, PTHREAD_MUTEX_INITIALIZER},
+  numconsumed = {0, PTHREAD_MUTEX_INITIALIZER};
 
-        execvp(cmd[0], cmd);
-        fprintf(stderr, "exec failed: %s\n", strerror(errno));
-        exit(1);
-    } else if (pid < 0) {
-        // Fork failed
-        perror("fork");
-    } else {
-        // Parent process
-        setpgid(pid, pid);
-        tcsetpgrp(sh->shell_terminal, pid);
+/*Shared queue that producers and consumers will access*/
+static queue_t pc_queue;
 
-        int status;
-        waitpid(pid, &status, WUNTRACED);
+/**
+ * Produces items at a random interval. Exits once it has produced
+ * the correct number of items.
+ */
+static void *producer(void *args)
+{
 
-        tcsetpgrp(sh->shell_terminal, sh->shell_pgid);
-        tcsetattr(sh->shell_terminal, TCSADRAIN, &sh->shell_tmodes);
-    }
+     int num = *((int *)args);
+     //pthread_t tid = pthread_self();
+     unsigned int seedp = 0;
+     struct timespec s = {0, 0};
+     int *itm = NULL;
+
+     // fprintf(stderr, "Producer thread: %ld - producing %d items\n", tid, num);
+     for (int i = 0; i < num; i++)
+     {
+          if (delay)
+          {
+               /*simulate producing the item*/
+               s.tv_nsec = (rand_r(&seedp) % MAX_SLEEP);
+               nanosleep(&s, NULL);
+          }
+
+          itm = (int *)malloc(sizeof(int));
+          *itm = i;
+          // Put the item into the queue
+          enqueue(pc_queue, itm);
+
+          // Update counters for testing purposes
+          pthread_mutex_lock(&numproduced.lock);
+          numproduced.num++;
+          pthread_mutex_unlock(&numproduced.lock);
+     }
+     // fprintf(stderr, "Producer thread: %ld - Done producing!\n", tid);
+     pthread_exit(NULL);
 }
 
-int main(int argc, char **argv) {
-    int opt;
-    long arg_max = sysconf(_SC_ARG_MAX);
-    if (arg_max == -1) {
-        perror("sysconf");
-        return 1;
-    }
+/**
+ * Consumes items.
+ */
+static void *consumer(void *args)
+{
+     UNUSED(args);
+     //pthread_t tid = pthread_self();
+     unsigned int seedp = 0;
+     struct timespec s = {0, 0};
+     int *itm = NULL;
+     // fprintf(stderr, "Consumer thread: %ld\n", tid);
 
-    while ((opt = getopt(argc, argv, "v")) != -1) {
-        switch (opt) {
-            case 'v':
-                printf("lab version %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
-                return 0;
-            default:
-                fprintf(stderr, "Usage: %s [-v]\n", argv[0]);
-                return 1;
-        }
-    }
+     while (true)
+     {
+          if (delay)
+          {
+               /*simulate producing the item*/
+               s.tv_nsec = (rand_r(&seedp) % MAX_SLEEP);
+               nanosleep(&s, NULL);
+          }
 
-    struct shell sh;
-    parse_args(argc, argv);
-    initialize_shell(&sh);
+          itm = (int *)dequeue(pc_queue);
+          if (itm)
+          {
+               free(itm);
+               itm = NULL;
+               // Update counters for testing purposes
+               pthread_mutex_lock(&numconsumed.lock);
+               numconsumed.num++;
+               pthread_mutex_unlock(&numconsumed.lock);
+          }
+          else
+          {
+               // If the queue is implemented correctly we should not
+               // get a NULL item during normal operation. It is possible to
+               // get a NULL item AFTER shutdown has been called which is fine
+               // because we are just cleaning up all the items.
+               if (!is_shutdown(pc_queue))
+               {
+                    fprintf(stderr, "ERROR: Got a null item when queue was not shutdown!\n");
+               }
+               break;
+          }
+     }
+     // fprintf(stderr, "Consumer Thread: %ld - Done consuming!\n", tid);
+     pthread_exit(NULL);
+}
 
-    char *prompt = getenv("MY_PROMPT");
-    if (prompt == NULL) {
-        prompt = "$ ";
-    }
-    sh.prompt = prompt;
+static void usage(char *n)
+{
+     fprintf(stderr, "Usage: %s [-c num consumer] [-p num producer] [-i num items] [-s queue size] <-d introduce delay>\n", n);
+     fprintf(stderr, "-d will introduce a random delay between consumer and producer");
+     exit(EXIT_FAILURE);
+}
 
-    using_history();
+int main(int argc, char *argv[])
+{
+     int nump = 1;       /*total number of producers*/
+     int numc = 1;       /*total number of consumers*/
+     int numitems = 10;  /*total number of items to produce per thread*/
+     int queue_size = 5; /*The default size of the queue*/
+     int c;
 
-    char *line;
-    while ((line = readline(sh.prompt))) {
-        add_history(line);
+     pthread_t producers[MAX_P];
+     pthread_t consumers[MAX_C];
 
-        char *trimmed_line = trim_white(line);
-        if (strlen(trimmed_line) == 0) {
-            free(line);
-            continue;
-        }
+     while ((c = getopt(argc, argv, "c:p:i:s:dh")) != -1)
+          switch (c)
+          {
+          case 'c':
+               numc = atoi(optarg);
+               break;
+          case 'p':
+               nump = atoi(optarg);
+               ;
+               break;
+          case 'i':
+               numitems = atoi(optarg);
+               break;
+          case 's':
+               queue_size = atoi(optarg);
+               break;
+          case 'd':
+               delay = true;
+               break;
+          case 'h':
+               usage(argv[0]);
+               break;
+          default: /* ? */
+               usage(argv[0]);
+          }
+     if (numc > MAX_C)
+          numc = MAX_C;
+     if (nump > MAX_P)
+          nump = MAX_P;
 
-        char **argv_parsed = cmd_parse(trimmed_line);
-        if (!do_builtin(&sh, argv_parsed)) {
-            launch_process(&sh, argv_parsed);
-        }
-        cmd_free(argv_parsed);
-        free(line);
-    }
+     int per_thread = numitems / nump;
+     fprintf(stderr, "Simulating %d producers %d consumers with %d items per thread and a queue size of %d\n", nump, numc, per_thread, queue_size);
+     // Start our timing
+     double end = 0;
+     double start = getMilliSeconds();
 
-    sh_destroy(&sh);
-    return 0;
+     // Initialize the queue for usage
+     pc_queue = queue_init(queue_size);
+     /*Create the producer threads*/
+     for (int i = 0; i < nump; i++)
+     {
+          pthread_create(&producers[i], NULL, producer, (void *)&per_thread);
+     }
+
+     fprintf(stderr, "Creating %d consumer threads\n", numc);
+     /*Create the consumer threads*/
+     for (int i = 0; i < numc; i++)
+     {
+          pthread_create(&consumers[i], NULL, consumer, (void *)NULL);
+     }
+
+     /*Wait for all the the producer threads to finish*/
+     for (int i = 0; i < nump; i++)
+     {
+          pthread_join(producers[i], NULL);
+     }
+
+     // Once all the producers are finished we set a flag so the consumer thread can finish up
+     // Once shutdown is called your queue should drain all remaining items and be read for
+     // destruction!
+     queue_shutdown(pc_queue);
+
+     /*Wait for all the the consumer threads to finish*/
+     for (int i = 0; i < numc; i++)
+     {
+          pthread_join(consumers[i], NULL);
+     }
+
+     if (numproduced.num != numconsumed.num)
+     {
+          fprintf(stderr, "ERROR! produced != consumed\n");
+          abort();
+     }
+     fprintf(stderr, "Queue is empty:%s\n", is_empty(pc_queue) ? "true" : "false");
+     fprintf(stderr, "Total produced:%d\n", numproduced.num);
+     fprintf(stderr, "Total consumed:%d\n", numconsumed.num);
+
+     // Free up all the stuff we allocated
+     queue_destroy(pc_queue);
+
+     // End our timing
+     end = getMilliSeconds();
+     // Print timing to standard out to graph
+     fprintf(stdout, " %f %d \n", end - start, numproduced.num);
+
+     return 0;
 }
